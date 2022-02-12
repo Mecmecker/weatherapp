605f7dc792af01bbf4134934fb666c20d4f70562
Daniel Muñoz <Danielromero@hotmail.com>
2022-02-12 11:23:41 +0100

    cambio en el main .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weatherapp/screens/screens.dart';
 import 'package:weatherapp/themes/themes.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
-  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
